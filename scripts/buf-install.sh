dcc1f1c14c9750b581632ae0153cc38b91fe8311
sweexordious <chamirachid1@gmail.com>
2024-10-07 21:42:25 +0400

    chore: temporary change12

--- scripts/buf-install.sh
+++ scripts/buf-install.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-PROJECT=nmt
-# Use your desired buf version
 BUF_VERSION=1.44.0
-# buf is installed to ~/bin/your-project-name.
-BIN_DIR=$HOME/bin/$PROJECT
 
 curl -sSL \
 	"https://github.com/bufbuild/buf/releases/download/v$BUF_VERSION/buf-$(uname -s)-$(uname -m)" \
-	-o "$BIN_DIR/buf"
-chmod +x "$BIN_DIR/buf"
+	-o "./buf"
+chmod +x "./buf"
